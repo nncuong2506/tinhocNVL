97f5cba99a69b88216b55080e8a61b54b4481125
NhatCuong <54887855+nncuong2506@users.noreply.github.com>
2025-10-24 22:11:23 +0700

    Update app.py

--- NVL/chat/app.py
+++ NVL/chat/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from chat import get_response
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -23,4 +24,5 @@ def predict():
     return jsonify({"answer": response})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
